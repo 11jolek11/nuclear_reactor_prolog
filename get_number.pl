2b5965c0882d1e0fd49b4ab823f0c04cc349befb
11jolek11 <dabrowskiandrzej094@gmail.com>
2023-10-31 21:10:40 +0100

    Add integer read from stdin Version with readutils module Adds reading integer and numbers (without typecheck)

--- get_number.pl
+++ get_number.pl
@@ -1,26 +1,39 @@
+use_module(library(readutil)). % Is it legal?
 
 /*
 Reads number from given Stream.
-
-Read bc it may be usefullllll for you Lukasz
-https://www.amzi.com/manuals/amzi/pro/ref_io.htm
-
 
 Streams in Prolog:
     user_input [0] - standard input device. Typically the console for environments that support consoles, e.g. Unix and the Windows console (old DOS box).
     user_output [1] - standard output device. Typically the console for environments that support consoles, e.g. Unix and the Windows console (old DOS box).
     user_error [2] - standard error device. Typically the console for environments that support consoles, e.g. Unix and the Windows console (old DOS box).
-    user_function [3] - Optional application defined stream. Used for special applications that need to create custom Prolog streams. See Logic Server API for details. See note below as well.
+    user_function [3] - Optional application defined stream. Used for special applications that need to create custom Prolog streams.
+
+Uses readutil module from swi-prolog.
 */
-read_integer_from_stream(N, Stream) :- 
-    read_line_to_codes(Stream, Codes),
-    number_codes(N, Codes),
-    !.
 
 /*
-Reads number from standard stream
+Reads integer from given Stream
 */
-%TODO(11jolek11): Check if work under Windows
+read_number_from_stream(N, Stream) :- 
+    is_stream(Stream) -> % If Stream is a existing stream do smth
+        read_line_to_codes(Stream, Codes),
+        number_codes(N, Codes),
+        !
+    ; % Else
+        type_error("Existing stream", Stream).
+
+
+read_integer_from_stream(N, Stream) :-
+    read_number_from_stream(N, Stream),
+    integer(N) -> true;
+        type_error("Integer", N).
+
+/*
+Reads integer from standard (system) stream
+*/
 read_integer(N) :-
     read_integer_from_stream(N, user_input).
 
+
+% TODO(11jolek11): Add reading floats
