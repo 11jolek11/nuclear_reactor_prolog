97503bc16fc891712deb307787b473ac0a61b68d
11jolek11 <dabrowskiandrzej094@gmail.com>
2023-11-08 21:26:34 +0000

    adds input handlers and messages

--- main.pl
+++ main.pl
@@ -18,7 +18,35 @@ start :-
 :- dynamic(store_ans/2)
 ask_user(Q, A) :- store_ans(Q, A).
 
+:- dynamic(reactor_number/1)
+ask_reactor_number(N) :- store_ans(Q, A).
+
 
 clear_answers :- retract(store_ans(_, _)), fail.
+  
+% ask_question(Trigger, Typ_pobieranych_danych, Result) :- 
+ask_question(Trigger, Pobieracz_danych, Result) :- 
+  user_message(Trigger),
+  call(Pobieracz_danych, Result).
 
-ask_question()
+decision_tree :-
+  % Pytanie 1
+  ask_question(q_reactor_number, read_integer, Result)
+  ask_user(q_reactor_number, Result)
+  % We do absolotely freaking nothing
+
+  % Pytanie 2
+  ask_question(q_are_there_errors, get_yes_or_no, Result)
+
+
+
+% dozymetry
+% Result - lista informacyjna o dozymetrze
+% Jeśli Result[0] in supported_brands && Result[1] in supported_models &&
+% int(Result[2]) > 10 ->
+%  ask_question(czy osłona uszkodzona);
+% eLsE ->
+%  ask_question(ile prętów))
+
+% unpack_ans(List, Brand, Model, Battery_level):-
+  
